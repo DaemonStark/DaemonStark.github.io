e401cde00fbb0aaae089469715394ec9ae031a3c
Ashutosh Deshpande <36955661+DaemonStark@users.noreply.github.com>
2021-03-12 00:59:22 +0530

    Bootstrap Ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
-/*!
-    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-    */
-    (function ($) {
+// JS 
+
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
